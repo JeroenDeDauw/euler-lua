0ebceac1a426e3612a11619e256835e009fb8783
jeroendedauw <jeroendedauw@gmail.com>
2012-06-30 16:25:49 +0200

    Made code for p26 work although I now realized its solving another problem :)

--- solutions/p26.lua
+++ solutions/p26.lua
@@ -4,11 +4,23 @@
 -- License: GNU GPL v2+
 
 function find_repeating_part( str )
-	for length=math.floor( string.len( str ) ) / 2, 2, -1 do
+	for length=math.floor( string.len( str ) / 2 ), 1, -1 do
+		charsRepeat = true
+		position = 0
+		base = string.sub( str, 1, length )
 
+		repeat
+			startPosition = position * length + 1
+			position = position + 1
+			endPosition = startPosition + length - 1
 
+			part = string.sub( str, startPosition, endPosition );
 
-		if false then
+			charsRepeat = string.sub( base, 1, string.len( part ) ) == part
+
+		until not charsRepeat or endPosition > string.len( str )
+
+		if charsRepeat then
 			return length
 		end
 	end
@@ -17,7 +29,36 @@ function find_repeating_part( str )
 end
 
 
-for i=1,1000,1 do
+tests = {}
+
+tests['1'] = 0
+tests['11'] = 1
+tests['111'] = 1
+tests['110'] = 0
+tests['1111'] = 2
+tests['11111'] = 2
+tests['11110'] = 0
+tests['1110'] = 0
+tests['1010'] = 2
+tests['10101'] = 2
+tests['10001'] = 0
+tests['010'] = 0
+tests['01'] = 0
+
+for str, expected in pairs(tests) do
+	actual = find_repeating_part( str )
+
+	if actual == expected then
+		error = ''
+	else
+		error = ' - ERROR'
+	end
+
+	print( str ..' - ' .. expected  .. ' - ' .. actual .. error )
+end
+
+
+for i=1,10,1 do
 
 	str = string.sub( 1 / i, 3, 14 )
 	print( str .. ' - ' .. find_repeating_part( str ) )
